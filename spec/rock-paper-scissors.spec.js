c3f9cebb93639e73ff75f4bbd577970d6a9b7de6
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-23 19:25:31 +0200

    created a src folder, added first test into spec.js

--- spec/rock-paper-scissors.spec.js
+++ spec/rock-paper-scissors.spec.js
@@ -1,1 +1,9 @@
-const { RockpaperScissors } = require( '.spec.helper')
+const { rockPaperScissor } = require( '.spec.helper')
+
+describe('Rock Paper Scissors', () => {
+    let rockPaperScissor = new RockPaperScissor
+})
+
+it('returns try again if player loses', () => {
+    expect(rockPaperScissor.check(rock)).to eql(paper)
+})
