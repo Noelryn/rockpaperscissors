987a32dd52bda5c64b575e5bfc5b08533ef312d4
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-24 21:07:37 +0200

    added feature for paper beating rock, rock beating scissors and scissors beat paper

--- spec/rock-paper-scissors.spec.js
+++ spec/rock-paper-scissors.spec.js
@@ -1,7 +1,7 @@
 const { RockPaperScissors } = require('./spec.helper')
 
 describe('Rock Paper Scissor', () => {
-    let rps = new RockPaperScissors()
+    let rps = new RockPaperScissors
 
     it('should return a tie when same object', () => {
         expect(rps.compare("rock", "rock")).to.eq("TIE!");
@@ -10,5 +10,12 @@ describe('Rock Paper Scissor', () => {
     it('should return paper beats rock', () => {
         expect(rps.compare("paper", "rock")).to.eq("Paper beats rock")
     })
-    
+
+    it('should return rock beats scissors', () => {
+        expect(rps.compare("rock", "scissors")).to.eq("Rock beats scissors")
+    })
+
+    it('should return scissors beats paper', () => {
+        expect(rps.compare("scissors", "paper")).to.eq("Scissors beats paper")
+    })
 });
--- src/js/rock-paper-scissor.js
+++ src/js/rock-paper-scissor.js
@@ -1,7 +1,13 @@
-function RockPaperScissors()  {
-    rps.compare = (a, a) => {
-        if (a === a) {
-            return "TIE!"; 
-        }
+//function RockPaperScissors()  {
+    //rps.compare = (a, a) => {
+        //if (a === a) {
+            //return "TIE!"; 
+        //}
+    //}
+//};
+
+let compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        return "TIE!";
     }
-};
+}
